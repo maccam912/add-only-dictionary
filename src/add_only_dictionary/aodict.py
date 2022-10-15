07ea29be5b184eafde0f64e07c8987c3ae9ad9f5
Matt Koski <maccam912@gmail.com>
2022-10-15 08:29:47 -0500

    fixed precommit

--- src/add_only_dictionary/aodict.py
+++ src/add_only_dictionary/aodict.py
@@ -1,3 +1,4 @@
+"""class AODict."""
 from collections import UserDict
 from typing import TypeVar
 
@@ -7,5 +8,8 @@ _VT = TypeVar("_VT")
 
 
 class AODict(UserDict[_KT, _VT]):
+    """AODict class."""
+
     def __setitem__(self, k: _KT, v: _VT) -> None:
+        """__setitem__."""
         super().__setitem__(k, v)
