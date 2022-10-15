74d61e94c67de1000f7f8d3489278ed8a66ff4a4
Matt Koski <maccam912@gmail.com>
2022-10-15 08:03:57 -0500

    update packages

--- src/add_only_dictionary/aodict.py
+++ src/add_only_dictionary/aodict.py
@@ -1,5 +1,10 @@
+from collections import UserDict
+from typing import TypeVar
+
+
 _KT = TypeVar("_KT")
 _VT = TypeVar("_VT")
+
 
 class AODict(UserDict[_KT, _VT]):
     def __setitem__(self, k: _KT, v: _VT) -> None:
