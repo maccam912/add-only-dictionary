5f3f22f3ba411cb4eabdf5fa61ebaf246bd50f3a
Matt Koski <maccam912@gmail.com>
2022-10-15 20:19:14 -0500

    fixed type

--- tests/test_aodict.py
+++ tests/test_aodict.py
@@ -4,5 +4,5 @@ from add_only_dictionary import AODict
 
 def test_instantiate() -> None:
     """Just a sanity check to see if AODict can be created."""
-    ao: AODict[str, str] = AODict()
+    ao: AODict[str, int] = AODict()
     ao["a"] = 1
