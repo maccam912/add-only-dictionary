1d82a69114cc57816eab6068c561a7f4893f4d2c
Matt Koski <maccam912@gmail.com>
2022-10-16 13:28:57 -0500

    convert dicts to aodicts

--- tests/test_aodict.py
+++ tests/test_aodict.py
@@ -1,17 +1,46 @@
 """Tests for AODict."""
+import json
+
 from add_only_dictionary import AODict
 
 
 def test_instantiate() -> None:
     """Just a sanity check to see if AODict works."""
-    ao: AODict[str, int] = AODict()
+    ao: AODict = AODict()
     ao["a"] = 1
     assert ao["a"] == 1
 
 
 def test_no_overwrite() -> None:
     """Make sure if the key is already in the dict, it can't be updated."""
-    ao: AODict[str, int] = AODict()
+    ao: AODict = AODict()
     ao["a"] = 1
     ao["a"] = 2
     assert ao["a"] == 1
+
+
+def test_convert_dict_to_aodict() -> None:
+    """If a dict is passed in to constructor, convert it to AODict."""
+    ao: AODict = AODict({"a": 1})
+    assert ao["a"] == 1
+    ao["a"] = 2
+    assert ao["a"] == 1
+
+
+def test_nested_dict() -> None:
+    """If a value for some key is a dict, convert it to an AODict."""
+    ao: AODict = AODict()
+    ao["a"] = {"b": 2}
+    assert ao["a"]["b"] == 2
+    ao["a"]["b"] = 3
+    assert ao["a"]["b"] == 2
+    ao["a"]["c"] = 3
+    assert ao["a"]["c"] == 3
+
+
+def test_json_dumps() -> None:
+    """When using json.dumps on an AODict it should behave like regular dict."""
+    ao: AODict = AODict()
+    ao["a"] = 1
+    s = json.dumps(ao)
+    assert s == '{"a": 1}'
